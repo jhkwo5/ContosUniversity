21fe82c996dcb199b9602be7096f5471fa1ef62c
jhkwo5 <jhkwo12@gmail.com>
2018-01-05 13:03:57 +1100

    modified database structures

--- ContosUniversity/Migrations/20180105014713_ComplexDataModel.Designer.cs
+++ ContosUniversity/Migrations/20180105014713_ComplexDataModel.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace ContosUniversity.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20180105014713_ComplexDataModel")]
+    partial class ComplexDataModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
